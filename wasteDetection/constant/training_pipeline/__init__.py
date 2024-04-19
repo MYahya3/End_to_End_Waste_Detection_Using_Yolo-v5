22d190c44f9e9812c0fbbea21309973b12736453
yahya <yahya@ifiveo.com>
2024-04-19 15:30:35 +0500

    Data ingestion added

---
+++ wasteDetection/constant/training_pipeline/__init__.py
@@ -0,0 +1,11 @@
+ARTIFACTS_DIR: str = "artifacts"
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+
+DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1EeMvkTEj_5J7VE5Ck5z83SmR4E7Pnc0l/view?usp=sharing"
+
