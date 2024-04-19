6c39870709b290ca2267837dafbda8514254d72a
yahya <yahya@ifiveo.com>
2024-04-19 17:06:08 +0500

    Model Trainer added

--- wasteDetection/constant/training_pipeline/__init__.py
+++ wasteDetection/constant/training_pipeline/__init__.py
@@ -1,5 +1,3 @@
-
-
 ARTIFACTS_DIR: str = "artifacts"
 
 """
@@ -9,7 +7,7 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
-DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1C53BqLZsI6c5LvTFK5awL24giY_ZCpWi/view?usp=sharing"
+DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1ts05krsfKH3hhpNnZXVyjgxD6zB4d-1T/view?usp=sharing"
 
 
 """
@@ -21,3 +19,15 @@ DATA_VALIDATION_DIR_NAME: str = "data_validation"
 DATA_VALIDATION_STATUS_FILE = 'status.txt'
 
 DATA_VALIDATION_ALL_REQUIRED_FILES = ["test", "train", "val", "data.yaml"]
+
+
+"""
+MODEL TRAINER related constant start with MODEL_TRAINER var name
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+
+MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov5s.pt"
+
+MODEL_TRAINER_NO_EPOCHS: int = 1
+
+MODEL_TRAINER_BATCH_SIZE: int = 16
